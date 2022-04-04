de23108bbf808d5020a78499ac007f64b2e1b3c2
Oleksandr-D <dolbnja9@gmail.com>
2022-04-04 13:09:18 +0300

    add products in basket

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import {
 import {
   IProductResponse
 } from 'src/app/shared/interfaces/products/products.interface';
+import { OrderService } from 'src/app/shared/services/order/order.service';
 import {
   ProductService
 } from 'src/app/shared/services/product/product.service';
@@ -16,9 +17,11 @@ import {
 })
 export class HomeComponent implements OnInit {
   public userProducts: Array < IProductResponse >= [];
-  public rols: Array < IProductResponse >= [];
+  
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private orderService:OrderService) {}
 
   ngOnInit(): void {
     this.loadProducts();
@@ -28,6 +31,33 @@ export class HomeComponent implements OnInit {
     this.productService.getAllByCategory('роли').subscribe(data => {
       this.userProducts = data;
     })
+  }
+
+  productCount(product: IProductResponse, value: boolean): void {
+    if (value) {
+      ++product.count;
+    } else if (!value && product.count > 1) {
+      --product.count;
+    }
+  }
+
+  //check if there is something in the basket add to local storage
+  addToBasket(product: IProductResponse): void {
+    let basket: Array < IProductResponse >= [];
+    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+      basket = JSON.parse(localStorage.getItem('basket') as string);
+      if (basket.some(prod => prod.id === product.id)) {
+        const index = basket.findIndex(prod => prod.id === product.id);
+        basket[index].count += product.count;
+      } else {
+        basket.push(product);
+      }
+    } else {
+      basket.push(product);
+    }
+    localStorage.setItem('basket', JSON.stringify(basket));
+    product.count = 1;
+     this.orderService.changeBasket.next(true);
   }
 
 
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -3,11 +3,20 @@ import {
   OnDestroy,
   OnInit
 } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router
+} from '@angular/router';
+import {
+  Subscription
+} from 'rxjs';
 import {
   IProductResponse
 } from 'src/app/shared/interfaces/products/products.interface';
+import {
+  OrderService
+} from 'src/app/shared/services/order/order.service';
 import {
   ProductService
 } from 'src/app/shared/services/product/product.service';
@@ -19,15 +28,16 @@ import {
 })
 export class ProductComponent implements OnInit, OnDestroy {
   public userProducts: Array < IProductResponse >= [];
-  private eventSubscription!:Subscription;
+  private eventSubscription!: Subscription;
 
   constructor(
     private productService: ProductService,
-    private activatedRoute:ActivatedRoute,
-    private router:Router
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private orderService: OrderService
   ) {
-     this.eventSubscription = this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd ) {
+    this.eventSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
         this.loadProducts();
       }
     })
@@ -39,11 +49,36 @@ export class ProductComponent implements OnInit, OnDestroy {
   }
 
   loadProducts(): void {
-    const categoryName = this.activatedRoute.snapshot.paramMap.get('category') as string; 
+    const categoryName = this.activatedRoute.snapshot.paramMap.get('category') as string;
     this.productService.getAllByCategory(categoryName).subscribe(data => {
       this.userProducts = data;
     })
   }
+  productCount(product: IProductResponse, value: boolean): void {
+    if (value) {
+      ++product.count;
+    } else if (!value && product.count > 1) {
+      --product.count;
+    }
+  }
 
+  //check if there is something in the basket add to local storage
+  addToBasket(product: IProductResponse): void {
+    let basket: Array < IProductResponse >= [];
+    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+      basket = JSON.parse(localStorage.getItem('basket') as string);
+      if (basket.some(prod => prod.id === product.id)) {
+        const index = basket.findIndex(prod => prod.id === product.id);
+        basket[index].count += product.count;
+      } else {
+        basket.push(product);
+      }
+    } else {
+      basket.push(product);
+    }
+    localStorage.setItem('basket', JSON.stringify(basket));
+    product.count = 1;
+    this.orderService.changeBasket.next(true);
+  }
 
 }
