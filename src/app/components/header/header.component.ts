5ec6f26cc4cf645c7c891a98079bcdae879fa8e3
Oleksandr-D <dolbnja9@gmail.com>
2022-04-04 19:34:27 +0300

    showing basket products

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,9 @@ import {
 import {
   IProductResponse
 } from 'src/app/shared/interfaces/products/products.interface';
-import { OrderService } from 'src/app/shared/services/order/order.service';
+import {
+  OrderService
+} from 'src/app/shared/services/order/order.service';
 
 @Component({
   selector: 'app-header',
@@ -14,12 +16,13 @@ import { OrderService } from 'src/app/shared/services/order/order.service';
 })
 export class HeaderComponent implements OnInit {
 
-  private basket: Array < IProductResponse > = [];
+  public basket: Array < IProductResponse > = [];
   public total = 0;
-  public count =0;
+  public count = 0;
+  public isOpen = false;
 
   constructor(
-    private orderService:OrderService
+    private orderService: OrderService
   ) {}
 
   ngOnInit(): void {
@@ -32,20 +35,24 @@ export class HeaderComponent implements OnInit {
       this.basket = JSON.parse(localStorage.getItem('basket') as string);
     }
     this.getTotalPrice();
+    console.log('Works ==>',this.basket);
+    
   }
 
-  getTotalPrice():void{
-   this.total = this.basket.reduce((total:number, prod:IProductResponse)=>
-   total + prod.count * prod.price, 0);
-   this.count = this.basket.reduce((total:number, prod:IProductResponse)=>
-   total + prod.count, 0);
+  getTotalPrice(): void {
+    this.total = this.basket.reduce((total: number, prod: IProductResponse) =>
+      total + prod.count * prod.price, 0);
+    this.count = this.basket.reduce((total: number, prod: IProductResponse) =>
+      total + prod.count, 0);
   }
 
-  updateBasket():void{
-  this.orderService.changeBasket.subscribe(()=>{
-    this.loadBasket();
-  })
-
+  updateBasket(): void {
+    this.orderService.changeBasket.subscribe(() => {
+      this.loadBasket();
+    })
+  }
+  openModal():void{
+      this.isOpen = !this.isOpen;
   }
 
 }
