70c5300d5e827216da31239ffa0075425b296391
Oleksandr-D <dolbnja9@gmail.com>
2022-09-29 17:58:28 +0300

    add firestore database

--- src/app/pages/authorization/authorization.component.ts
+++ src/app/pages/authorization/authorization.component.ts
@@ -1,41 +1,41 @@
 import {
   Component,
+  OnDestroy,
   OnInit
 } from '@angular/core';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
-import {
-  Router
-} from '@angular/router';
-import {
-  ROLE
-} from 'src/app/shared/constants/role.constants';
+import { doc, docData, Firestore } from '@angular/fire/firestore';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { ROLE } from 'src/app/shared/constants/role.constants';
 
-import {
-  AccountService
-} from 'src/app/shared/services/account/account.service';
+import { AccountService } from 'src/app/shared/services/account/account.service';
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
   styleUrls: ['./authorization.component.scss']
 })
-export class AuthorizationComponent implements OnInit {
+export class AuthorizationComponent implements OnInit, OnDestroy {
   public authForm!: FormGroup;
+  public loginSubscription!:Subscription;
 
   constructor(
     private fb: FormBuilder,
     private accountService: AccountService,
     private router: Router,
-    private auth:Auth
+    private auth:Auth,
+    private afs: Firestore,
+    private toastr:ToastrService
   ) {}
 
   ngOnInit(): void {
     this.initAuthForm();
+  }
+  ngOnDestroy():void{
+    this.loginSubscription.unsubscribe();
   }
 
   initAuthForm(): void {
@@ -62,14 +62,28 @@ export class AuthorizationComponent implements OnInit {
     // })
     const {email, password} = this.authForm.value;
     this.login(email, password).then(() => {
-      console.log('LOgin Done');
+      this.toastr.success('Користувач успішно увішов');
     }).catch(e => {
-      console.log ('login error', e);
+     this.toastr.error(e.message);
     })
   }
 
    async login(email:string, password:string):Promise <void>{
     const credential = await signInWithEmailAndPassword(this.auth, email , password);
+    this.loginSubscription = docData(doc(this.afs, 'Users', credential.user.uid)).subscribe(user =>{
+      console.log('user', user)
+      const currentUser = {...user, uid:credential.user.uid};
+      localStorage.setItem('currentUser', JSON.stringify(currentUser));
+      if (user && user.role === ROLE.USER){
+        this.router.navigate(['/userprofile'])
+      }else if (user && user.role === ROLE.ADMIN){
+        this.router.navigate(['/admin']);
+      }
+      this.accountService.isUserLogin$.next(true);
+   },(e)=>{
+       console.log('error', e)
+      })
+
    }
 
 
