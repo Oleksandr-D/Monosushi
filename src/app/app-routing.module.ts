e333222712c5827a492e058caaae780677293fd7
Oleksandr-D <dolbnja9@gmail.com>
2023-01-13 10:20:42 +0200

    added about, discount modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { DiscountComponent } from './pages/discount/discount.component';
-import { DiscountInfoComponent } from './pages/discount-info/discount-info.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductInfoComponent } from './pages/product-info/product-info.component';
 import { DeliveryComponent } from './pages/delivery/delivery.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { AboutComponent } from './pages/about/about.component';
 import { OffertaComponent } from './pages/offerta/offerta.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ProductInfoResolver } from './shared/services/product/product-info.resolver';
-import { DiscountInfoResolver } from './shared/services/discount/discount-info.resolver';
 import { AuthGuard } from './shared/guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'discount', component: DiscountComponent },
-  {
-    path: 'discount/:id',
-    component: DiscountInfoComponent,
-    resolve: { discountInfo: DiscountInfoResolver },
+  { path: 'discount',
+    loadChildren: () => import('./pages/discount/discount.module').then(m => m.DiscountModule)
   },
   { path: 'product/:category', component: ProductComponent },
   {
@@ -31,15 +24,16 @@ const routes: Routes = [
   },
   { path: 'delivery', component: DeliveryComponent },
   { path: 'payment', component: PaymentComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about',
+    loadChildren:() => import('./pages/about/about.module').then(m => m.AboutModule),
+  },
   { path: 'offerta', component: OffertaComponent },
   { path: 'checkout', component: CheckoutComponent },
   {
     path: 'auth',
     loadChildren: () =>
       import('./pages/authorization/authorization.module').then(
-        (m) => m.AuthorizationModule
-      ),
+        (m) => m.AuthorizationModule),
   },
   {
     path: 'user-profile',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { DiscountComponent } from './pages/discount/discount.component';
-import { DiscountInfoComponent } from './pages/discount-info/discount-info.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductInfoComponent } from './pages/product-info/product-info.component';
 import { DeliveryComponent } from './pages/delivery/delivery.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { AboutComponent } from './pages/about/about.component';
 import { OffertaComponent } from './pages/offerta/offerta.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { AuthDialogComponent } from './components/auth-dialog/auth-dialog.component';
@@ -39,13 +36,10 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    DiscountComponent,
-    DiscountInfoComponent,
     ProductComponent,
     ProductInfoComponent,
     DeliveryComponent,
     PaymentComponent,
-    AboutComponent,
     OffertaComponent,
     CheckoutComponent,
     AuthDialogComponent,
