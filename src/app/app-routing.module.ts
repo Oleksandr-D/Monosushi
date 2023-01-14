f27d2cd677792305810df6d8cfb28ced6cb5397c
Oleksandr-D <dolbnja9@gmail.com>
2023-01-14 10:08:40 +0200

    aded delivery, home, oferta components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductInfoComponent } from './pages/product-info/product-info.component';
-import { DeliveryComponent } from './pages/delivery/delivery.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { OffertaComponent } from './pages/offerta/offerta.component';
 import { ProductInfoResolver } from './shared/services/product/product-info.resolver';
 import { AuthGuard } from './shared/guards/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'discount',
-    loadChildren: () => import('./pages/discount/discount.module').then(m => m.DiscountModule)
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'discount',
+    loadChildren: () =>
+      import('./pages/discount/discount.module').then((m) => m.DiscountModule),
   },
   { path: 'product/:category', component: ProductComponent },
   {
@@ -21,20 +24,33 @@ const routes: Routes = [
     component: ProductInfoComponent,
     resolve: { productInfo: ProductInfoResolver },
   },
-  { path: 'delivery', component: DeliveryComponent },
-  { path: 'payment', component: PaymentComponent },
-  { path: 'about',
-    loadChildren:() => import('./pages/about/about.module').then(m => m.AboutModule),
+  {
+    path: 'delivery',
+    loadChildren: () =>
+      import('./pages/delivery/delivery.module').then((m) => m.DeliveryModule),
   },
-  { path: 'offerta', component: OffertaComponent },
-  { path: 'checkout',
-    loadChildren: () => import('./pages/checkout/checkout.module') .then(m => m.CheckoutModule),
+  { path: 'payment', component: PaymentComponent },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./pages/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'offerta',
+    loadChildren: () =>
+      import('./pages/offerta/offerta.module').then((m) => m.OffertaModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./pages/checkout/checkout.module').then((m) => m.CheckoutModule),
   },
   {
     path: 'auth',
     loadChildren: () =>
       import('./pages/authorization/authorization.module').then(
-        (m) => m.AuthorizationModule),
+        (m) => m.AuthorizationModule
+      ),
   },
   {
     path: 'user-profile',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,9 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProductInfoComponent } from './pages/product-info/product-info.component';
-import { DeliveryComponent } from './pages/delivery/delivery.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { OffertaComponent } from './pages/offerta/offerta.component';
 import { AuthDialogComponent } from './components/auth-dialog/auth-dialog.component';
 import { PersonalDataComponent } from './pages/user-profile/personal-data/personal-data.component';
 import { OrderHistoryComponent } from './pages/user-profile/order-history/order-history.component';
@@ -34,12 +31,9 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     ProductComponent,
     ProductInfoComponent,
-    DeliveryComponent,
     PaymentComponent,
-    OffertaComponent,
     AuthDialogComponent,
     PersonalDataComponent,
     OrderHistoryComponent
