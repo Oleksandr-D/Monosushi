b361dbf33dbe349ca6177b55b68e875c70ac3381
Oleksandr-D <dolbnja9@gmail.com>
2023-01-20 19:13:08 +0200

    added password change component

--- src/app/pages/user-profile/user-profile-routing.module.ts
+++ src/app/pages/user-profile/user-profile-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
     children: [
       { path: 'personal-data', component: PersonalDataComponent },
       { path: 'order-history', component: OrderHistoryComponent },
+      { path: 'password-change', component: PasswordChangeComponent },
       { path: '', pathMatch: 'full', redirectTo: 'personal-data' },
     ],
   },
--- src/app/pages/user-profile/user-profile.module.ts
+++ src/app/pages/user-profile/user-profile.module.ts
@@ -5,12 +5,14 @@ import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { PersonalDataComponent } from './personal-data/personal-data.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
 
 @NgModule({
   declarations: [
     UserProfileComponent,
     OrderHistoryComponent,
     PersonalDataComponent,
+    PasswordChangeComponent,
   ],
   imports: [CommonModule, UserProfileRoutingModule, SharedModule],
 })
