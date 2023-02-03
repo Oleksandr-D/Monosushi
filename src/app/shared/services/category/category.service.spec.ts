76f89b0a1edcf5e7b982f98daf7ba26066fb8e2e
Oleksandr-D <dolbnja9@gmail.com>
2023-02-03 11:09:38 +0200

    added unit tests. test for CategoryService 'can test HttpClient.get'

--- src/app/shared/services/category/category.service.spec.ts
+++ src/app/shared/services/category/category.service.spec.ts
@@ -1,21 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CategoryService } from './category.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule
-      ],
+      imports: [HttpClientTestingModule],
+      providers: [CategoryService],
     });
     service = TestBed.inject(CategoryService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('can test HttpClient.get', () => {
+    const data = [
+      {
+        id: 2,
+        name: 'string',
+        path: 'string',
+        imagePath: 'string',
+      },
+    ];
+
+    service.getAll().subscribe((response) => expect(response).toBe(data));
+
+    const req = httpTestingController.expectOne('http://localhost:3000/categories');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(data);
+  });
+
+  afterEach(() => httpTestingController.verify());
 });
